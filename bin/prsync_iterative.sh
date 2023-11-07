45916e0a8e295368bb5b8d29f6de74da9f34730c
Amanda Ibsen <amanda.ibsen@gmail.com>
2023-11-07 14:44:49 +0000

    modified parallel rsync script so logs an number of jobs can be given as inputs

--- bin/prsync_iterative.sh
+++ bin/prsync_iterative.sh
@@ -1,29 +1,57 @@
 #!/bin/bash
-echo "running rsync in parallel" >> result_transfer_vhs_check_0.log
+echo "running rsync in parallel"
 
-#start_time=$(date +%s)
+usage() {
+  echo "Usage: $0 SOURCEDIR TARGETDIR LOGFILE TRANSFERFILE BATCHPREFIX njobs"
+  echo "  SOURCEDIR     : Source directory to find files. Must be provided"
+  echo "  TARGETDIR     : Target directory to rsync files. Must be provided"
+  echo "  LOGFILE       : Log file to write the elapsed time (default: log.out)"
+  echo "  TRANSFERFILE  : File to store list of files to transfer (default: transfer_files.out)."
+  echo "  BATCHPREFIX   : Prefix for batch files (default: batch)."
+  echo "  njobs         : Number of jobs to run in parallel (default: 2)."
+  exit 1
+}
 
-SOURCEDIR="$1"
-TARGETDIR="$2"
-njobs="$3"
+# Check if the help argument is provided
+if [ "$1" == "--help" ]; then
+  usage
+fi
 
-#find "$SOURCEDIR" -type f >> transfer_files_vhs_check.log
-#split -n "$njobs" transfer_files_vhs_check.log batch_vhs
-
-#end_time=$(date +%s)
-#duration=$((end_time - start_time))
-
-#echo "Elapsed Time listing files: $duration seconds" >> result_transfer_vhs_check.log
+# Check if the required arguments are provided
+if [ -z "$1" ] || [ -z "$2" ]; then
+  usage
+fi
 
 start_time=$(date +%s)
 
-for f in batch_vhs*; do
-#	logfile="log-$f.log"
-	rsync -ar --stats  --inplace --human-readable --info=progress2  --files-from="$f" "/" "$TARGETDIR" &
+SOURCEDIR="$1"
+TARGETDIR="$2"
+LOGFILE="${3:-log.out}"
+TRANSFERFILE="${4:-transfer_files.out}"
+BATCHPREFIX="${5:-batch}"
+njobs="${6:-2}"
+
+> "$TRANSFERFILE"
+find "$SOURCEDIR" -type f >> "$TRANSFERFILE"
+split -n l/"$njobs" "$TRANSFERFILE" "$BATCHPREFIX"
+
+end_time=$(date +%s)
+duration=$((end_time - start_time))
+
+echo "Elapsed Time listing files: $duration seconds" >> "$LOGFILE"
+
+start_time=$(date +%s)
+
+job_number=0
+for f in "$BATCHPREFIX"*; do
+	# logfile="njobs-$njobs-job-$f-$LOGFILE"
+	job_logfile="njobs-${njobs}-job-${job_number}-${LOGFILE}"
+	rsync -rL --stats  --inplace --human-readable --files-from="$f" "/" "$TARGETDIR" &> "$job_logfile" &
+	((job_number++))
 done;
 wait 
 
 end_time=$(date +%s)
 duration=$((end_time - start_time))
 
-echo "Elapsed Time transferring files: $duration seconds" >> result_transfer_vhs_check_0.log
+echo "Elapsed Time transferring files: $duration seconds" >> "$LOGFILE"
--- bin/rsync_simple.sh
+++ bin/rsync_simple.sh
@@ -8,7 +8,7 @@ SOURCEDIR="$1"
 TARGETDIR="$2"
 LOGFILE="$3"
 
-rsync -aL --stats --human-readable --inplace "$SOURCEDIR" "$TARGETDIR" > "$LOGFILE" 2>&1
+rsync -rL --stats --human-readable --inplace "$SOURCEDIR" "$TARGETDIR" > "$LOGFILE" 2>&1
 
 end_time=$(date +%s)
 duration=$((end_time - start_time))
