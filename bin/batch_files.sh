f85f8a41e1b2bd424f26f4c0fc91ff086eaf87a1
Amanda Ibsen <amanda.ibsen@gmail.com>
2023-11-13 14:28:03 +0000

    minor changes in script for batching

--- bin/batch_files.sh
+++ bin/batch_files.sh
@@ -2,11 +2,9 @@
 echo "creating batches from list of files"
 
 usage() {
-  echo "Usage: $0 SOURCEDIR TARGETDIR LOGFILE TRANSFERFILE BATCHPREFIX njobs"
-  echo "  SOURCEDIR     : Source directory to find files. Must be provided"
-  echo "  TARGETDIR     : Target directory to rsync files. Must be provided"
+  echo "Usage: $0 TARGETDIR LOGFILE LISTOFFILES BATCHPREFIX njobs"
   echo "  LOGFILE       : Log file to write the elapsed time (default: log.out)"
-  echo "  TRANSFERFILE  : File with list of files to transfer (default: transfer_files.out)."
+  echo "  LISTOFFILES  : File with list of files to transfer (default: transfer_files.out)."
   echo "  BATCHPREFIX   : Prefix for batch files (default: batch)."
   echo "  njobs         : Number of jobs to run in parallel (default: 2)."
   exit 1
@@ -24,14 +22,12 @@ fi
 
 start_time=$(date +%s)
 
-SOURCEDIR="$1"
-TARGETDIR="$2"
-LOGFILE="${3:-log.out}"
-TRANSFERFILE="${4:-transfer_files.out}"
-BATCHPREFIX="${5:-batch}"
-njobs="${6:-2}"
+LOGFILE="${1:-log.out}"
+LISTOFFILES="${2}"
+BATCHPREFIX="${3:-batch}"
+njobs="${4:-2}"
 
-split -n l/"$njobs" "$TRANSFERFILE" "$BATCHPREFIX"
+split -n l/"$njobs" "$LISTOFFILES" "$BATCHPREFIX"
 
 end_time=$(date +%s)
 duration=$((end_time - start_time))
