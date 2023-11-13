dbb2f85d52a0d5f9bce1f95cf5f2af938a50e0cd
Amanda Ibsen <amanda.ibsen@gmail.com>
2023-11-13 14:39:59 +0000

    cleaned up parallel sync scrip

--- bin/prsync_iterative.sh
+++ bin/prsync_iterative.sh
@@ -2,11 +2,10 @@
 echo "running rsync in parallel"
 
 usage() {
-  echo "Usage: $0 SOURCEDIR TARGETDIR LOGFILE TRANSFERFILE BATCHPREFIX njobs"
+  echo "Usage: $0 SOURCEDIR TARGETDIR LOGFILE BATCHPREFIX njobs"
   echo "  SOURCEDIR     : Source directory to find files. Must be provided"
   echo "  TARGETDIR     : Target directory to rsync files. Must be provided"
   echo "  LOGFILE       : Log file to write the elapsed time (default: log.out)"
-  echo "  TRANSFERFILE  : File with list of files to transfer (default: transfer_files.out)."
   echo "  BATCHPREFIX   : Prefix for batch files (default: batch)."
   echo "  njobs         : Number of jobs to run in parallel (default: 2)."
   exit 1
@@ -27,23 +26,12 @@ start_time=$(date +%s)
 SOURCEDIR="$1"
 TARGETDIR="$2"
 LOGFILE="${3:-log.out}"
-TRANSFERFILE="${4:-transfer_files.out}"
-BATCHPREFIX="${5:-batch}"
-njobs="${6:-2}"
+BATCHPREFIX="${4:-batch}"
+njobs="${5:-2}"
 
-#> "$TRANSFERFILE"
-#find "$SOURCEDIR" -type f >> "$TRANSFERFILE"
-#split -n l/"$njobs" "$TRANSFERFILE" "$BATCHPREFIX"
-#end_time=$(date +%s)
-#duration=$((end_time - start_time))
-
-#echo "Elapsed Time splitting files: $duration seconds" >> "$LOGFILE"
-
-#start_time=$(date +%s)
 
 job_number=0
 for f in "$BATCHPREFIX"*; do
-	# logfile="njobs-$njobs-job-$f-$LOGFILE"
 	job_logfile="njobs-${njobs}-job-${job_number}-${LOGFILE}"
 	rsync -rL --stats  --inplace --human-readable --files-from="$f" "$SOURCEDIR" "$TARGETDIR" &> "$job_logfile" &
 	((job_number++))
