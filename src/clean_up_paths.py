598b21c00555b3d9906adabc4499fcbfe6164744
Amanda Ibsen <amanda.ibsen@gmail.com>
2023-11-13 15:42:31 +0000

    apparently the list of files contains errors

--- src/clean_up_paths.py
+++ src/clean_up_paths.py
@@ -1,14 +1,25 @@
 import sys
 import glob
 
-def main(input_file_prefix, path_to_remove):
+def clean_up_paths(input_prefix, path_to_remove):
     pattern = f"{input_file_prefix}*"
+    weird_files=0
     for input_file in glob.glob(pattern):
         output_file = "new-"+input_file
-        with open(input_file, 'r') as infile, open(output_file, 'w') as outfile:
+        with open(input_file, 'r') as infile, open(output_file, 'w') as outfile, open(input_prefix+"_error.log", 'w') as error_log:
             for line in infile:
-                new_line = line.split(path_to_remove)[-1]
-                outfile.write(new_line + '\n')
+                split_lines = line.split(path_to_remove)
+                if (path_to_remove!=split_lines[0]):
+                    weird_file = line
+                    error_log.write(weird_file+'\n')
+                    weird_files+=1
+                else:    
+                    new_line = split_lines[-1]
+                    outfile.write(new_line + '\n')
+    print("there were "+str(weird_files)+" weird files")
+
+def main(input_prefix, path_to_remove):
+    clean_up_paths(input_prefix,path_to_remove)
 
 
 if __name__ == "__main__":
